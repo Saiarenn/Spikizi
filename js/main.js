eedb9bdfdedef7d6777f17b3f1a5a1ed23893fd8
Saiarenn <skane53814@gmail.com>
2022-12-06 00:12:36 +0600

    Update

--- js/main.js
+++ js/main.js
@@ -2,24 +2,24 @@
 const menuToggle = document.querySelector('.menu-toggle');
 const siteMenu = document.querySelector('.menu');
 
-menuToggle.addEventListener('click', () => {
-    const isOpened = menuToggle.getAttribute('aria-expanded') === "true";
-    if (isOpened ? closeMenu() : openMenu());
-});
+// menuToggle.addEventListener('click', () => {
+//     const isOpened = menuToggle.getAttribute('aria-expanded') === "true";
+//     if (isOpened ? closeMenu() : openMenu());
+// });
 
-function openMenu() {
-    menuToggle.setAttribute('aria-expanded', "true");
-    siteMenu.setAttribute('data-state', "opened");
-}
+// function openMenu() {
+//     menuToggle.setAttribute('aria-expanded', "true");
+//     siteMenu.setAttribute('data-state', "opened");
+// }
 
-function closeMenu() {
-    menuToggle.setAttribute('aria-expanded', "false");
-    siteMenu.setAttribute('data-state', "closing");
+// function closeMenu() {
+//     menuToggle.setAttribute('aria-expanded', "false");
+//     siteMenu.setAttribute('data-state', "closing");
 
-    siteMenu.addEventListener('animationend', () => {
-        siteMenu.setAttribute('data-state', "closed");
-    }, { once: true })
-}
+//     siteMenu.addEventListener('animationend', () => {
+//         siteMenu.setAttribute('data-state', "closed");
+//     }, { once: true })
+// }
 
 
 //slider
